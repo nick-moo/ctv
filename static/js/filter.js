7a99065fb6260cb379424325ea1542b4beb658d0
nick-moo <nick@moomedia.com.au>
2017-08-29 12:32:34 +1000

    Add files via upload

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,72 +1,31 @@
-function myFunction() {
-    // Declare variables
+$("a.download").click(function(selection) {
+   
     var input, filter, props, i;
-    input = document.getElementById('myInput');
-    filter = input.options[input.selectedIndex].value;
-    props = document.getElementsByClassName("col-half");
 
-    if (filter == 'showall') {
+        input = document.getElementById('myInput');
+        filter = selection.srcElement.getAttribute("data-select");
+        props = document.getElementsByClassName("col-third");
 
-        for (i = 0; i < props.length; i++) {
-            props[i].style.display = "";
-        }
+        if (filter == 'showall') {
 
-    } else {
-
-        for (i = 0; i < props.length; i++) {
-            if (props[i].classList.contains(filter)) {
+            for (i = 0; i < props.length; i++) {
                 props[i].style.display = "";
-            } else {
-                props[i].style.display = "none";
             }
+
+        } else {
+
+            for (i = 0; i < props.length; i++) {
+                if (props[i].classList.contains(filter)) {
+                    props[i].style.display = "";
+                } else {
+                    props[i].style.display = "none";
+                }
+            }
+            
         }
-        
-    }
-}
 
-/* ===== Logic for creating fake Select Boxes ===== */
-$('.sel').each(function() {
-  $(this).children('select').css('display', 'none');
-  
-  var $current = $(this);
-  
-  $(this).find('option').each(function(i) {
-    if (i == 0) {
-      $current.prepend($('<div>', {
-        class: $current.attr('class').replace(/sel/g, 'sel__box')
-      }));
-      
-      var placeholder = $(this).text();
-      $current.prepend($('<span>', {
-        class: $current.attr('class').replace(/sel/g, 'sel__placeholder'),
-        text: placeholder,
-        'data-placeholder': placeholder
-      }));
-      
-      return;
-    }
-    
-    $current.children('div').append($('<span>', {
-      class: $current.attr('class').replace(/sel/g, 'sel__box__options'),
-      text: $(this).text()
-    }));
-  });
+   $("#dlDropDown").dropdown("toggle");
+
 });
 
-// Toggling the `.active` state on the `.sel`.
-$('.sel').click(function() {
-  $(this).toggleClass('active');
-});
 
-// Toggling the `.selected` state on the options.
-$('.sel__box__options').click(function() {
-  var txt = $(this).text();
-  var index = $(this).index();
-  
-  $(this).siblings('.sel__box__options').removeClass('selected');
-  $(this).addClass('selected');
-  
-  var $currentSel = $(this).closest('.sel');
-  $currentSel.children('.sel__placeholder').text(txt);
-  $currentSel.children('select').prop('selectedIndex', index + 1);
-});
